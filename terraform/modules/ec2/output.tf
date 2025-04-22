9eda64521c8a6615c30f7d45700213319dec100c
Tadeo Alvarez <tadeo.regalado.alvarez@gmail.com>
2025-04-21 19:44:36 -0600

    EC2 fixing output

--- terraform/modules/ec2/output.tf
+++ terraform/modules/ec2/output.tf
@@ -1,11 +1,14 @@
-output "ec2_public_ip" {
-  value = aws_instance.ec2.public_ip
+output "ec2_public_ips" {
+  description = "Public IPs of all EC2 instances"
+  value       = [for instance in aws_instance.ec2 : instance.public_ip]
 }
 
-output "ec2_name" {
-  value = aws_instance.ec2
+output "ec2_key_names" {
+  description = "Key names of all EC2 instances"
+  value       = [for ec2 in aws_instance.ec2 : ec2.key_name]
 }
 
-output "ec2_type" {
-  value = aws_instance.ec2.ami
+output "ec2_amis" {
+  description = "AMIs used for all EC2 instances"
+  value       = [for ec2 in aws_instance.ec2 : ec2.ami]
 }
